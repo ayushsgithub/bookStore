f52eb580c6b43b09f04f84828111b4fa8d2273c2
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-07 16:58:02 +0530

    routes added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import "dotenv/config"
 import mongoose from "mongoose";
 import { Book } from "./model/bookModel.js";
-// import booksRoute from "./routes/booksRoute.js";
-// import cors from "cors";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const mongoDBURL=process.env.MONGO_URL
@@ -12,100 +12,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/books", async (req, res) => {
-    try {
-        if (
-            !req.body.title || !req.body.author || !req.body.publishYear
-        ) {
-            res.status(400).send({ message: "Send all the required fields: title, author, publishYear" })
-        }
-
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear,
-        }
-
-        const book = await Book.create(newBook)
-        res.status(201).send(book)
-        
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message })
-        
-    }
-})
-
-app.get("/books", async (req, res) => {
-    try {
-        const books = await Book.find({})
-        res.status(200).json({
-            count: books.length,
-            data: books,
-        })
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message })
-    }
-})
-
-app.get('/books/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const book = await Book.findById(id);
-  
-      return res.status(200).json(book);
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).send({ message: error.message });
-    }
-  });
-
-  app.put('/books/:id', async (req, res) => {
-    try {
-      if (
-        !req.body.title ||
-        !req.body.author ||
-        !req.body.publishYear
-      ) {
-        return res.status(400).send({
-          message: 'Send all required fields: title, author, publishYear',
-        });
-      }
-  
-      const { id } = req.params;
-  
-      const result = await Book.findByIdAndUpdate(id, req.body);
-  
-      if (!result) {
-        return res.status(404).json({ message: 'Book not found' });
-      }
-  
-      return res.status(200).send({ message: 'Book updated successfully' });
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).send({ message: error.message });
-    }
-  });
-
-
-  app.delete('/books/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const result = await Book.findByIdAndDelete(id);
-  
-      if (!result) {
-        return res.status(404).json({ message: 'Book not found' });
-      }
-  
-      return res.status(200).send({ message: 'Book deleted successfully' });
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).send({ message: error.message });
-    }
-  });
+app.use("/books", booksRoute);
 
 mongoose.connect(mongoDBURL)
     .then(() => {
