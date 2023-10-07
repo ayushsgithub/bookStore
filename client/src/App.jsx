d378272ed9a77e404a20710a8dd837685c3ba483
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-07 18:32:48 +0530

    pages, components & router implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBooks";
+import EditBooks from "./pages/EditBooks";
+import DeleteBooks from "./pages/DeleteBooks";
+import ShowBooks from "./pages/ShowBooks";
 
-
-function App() {
-
+const App = () => {
   return (
-  <>
-  <h1 className='bg-red-500'>hello</h1>
-  </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/books/create" element={<CreateBooks />}/>
+      <Route path="/books/details/:id" element={<ShowBooks />}/>
+      <Route path="/books/edit/:id" element={<EditBooks />}/>
+      <Route path="/books/delete/:id" element={<DeleteBooks />}/>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
