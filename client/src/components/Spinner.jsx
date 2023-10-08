7c4bfd9d778f5510b3d5a477939ff0793e4c47ee
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-08 18:16:26 +0530

    readme mod

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div class="flex justify-center items-center h-screen">
-      <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-sky-500"></div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-sky-500"></div>
     </div>
   );
 };
